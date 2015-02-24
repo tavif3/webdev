d686eae38e7d46f86ed2575cf94b64d6378c93f0
Tavi <octavian.iacovache@gmail.com>
2015-02-24 10:29:36 +0100

    Filtering working

--- shop_new/dev/products/products.controller.js
+++ shop_new/dev/products/products.controller.js
@@ -10,8 +10,31 @@
 			$scope.products = data;
 		}
 
+		var modelCategories = function(data){
+			$scope.categories = data;
+		}
+
 		productsService.getProducts()
 			.then(modelProducts);
+
+		productsService.getCategories()
+			.then(modelCategories);
+
+		var updateCategoriesSelected = function(){
+			$scope.categoriesSelected = productsService.getCategoriesSelected();
+		}
+
+		$scope.productFilter = function(product){
+			return productsService.productFilter(product);
+		}
+
+		$scope.categoryChange = function(category){
+			productsService.categoryChange(category);
+			updateCategoriesSelected();
+		}
+
+		updateCategoriesSelected();
+
 	}
 
 }());
--- shop_new/dev/products/products.service.js
+++ shop_new/dev/products/products.service.js
@@ -2,7 +2,8 @@
 
 	var productsService = function($http){
 
-		
+			var categoriesSelected = [],
+			products = [];
 		
 		var getProducts = function(){
 			return $http.get("data/products.json")
@@ -17,6 +18,7 @@
 							return findProductInArray(response.data, id);
 						})
 		}
+      
 		
 		var findProductInArray = function(data, id){
 			return data.filter(function(element){
@@ -25,10 +27,46 @@
 				}
 			});
 		}
+		
+		var getCategories = function(){
+			return $http.get("data/categories.json")
+						.then(function(response){
+							return response.data;
+						})
+		};
+
+		var getCategoriesSelected = function(){
+      		return categoriesSelected;
+      	}
+
+		var categoryChange = function(category){
+			var i = categoriesSelected.indexOf(category);
+            if (i > -1) {
+                categoriesSelected.splice(i, 1);
+            } 
+            else {
+                categoriesSelected.push(category);
+            }
+
+        };
+
+        var productFilter = function(product){
+            if (categoriesSelected.length > 0) {
+                if (categoriesSelected.indexOf(product.category) < 0){
+                    return;
+                }
+            }
+            return product;
+        }  
+
 
 		return {
 			getProducts: getProducts,
-			getProduct: getProduct
+			getProduct: getProduct,
+			getCategories: getCategories,
+			productFilter: productFilter,
+			categoryChange: categoryChange,
+			getCategoriesSelected: getCategoriesSelected
 		}
 
 	}
